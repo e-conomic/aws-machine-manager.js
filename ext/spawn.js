88917c620bfb6a4ccd3dc313598c3d701a3d0e1c
gaggle <mail@jonlauridsen.com>
2016-03-16 10:27:02 +0100

    fixed found by live integration
    To me it looks like mockgoose can be subtly different from real mongo, possibly due to version differences since production database on compose.io is not the latest. Whatever the cause, once I used this library in our production I ran into issues, so these changes are based on actual production requirements. It sucks that I can't find a way to test the changes, but e.g. mockgoose simply returns a proper promise on obj.update(), but that didn't seem to be the case in prod.. so what's a guy to do but make the necessary changes and commit?

--- ext/spawn.js
+++ ext/spawn.js
@@ -13,90 +13,61 @@ Manager.spawn = function (opts, userData) {
   var self = this
   opts = opts || {}
 
-  return self.machineExists(opts.name)
-    .then(function (exists) {
-      if (exists) throw new Error("Machine " + opts.name + " already exists")
-    })
-    .then(function () {
+  return Q.fcall(function () {
       var obj = new self.db.Machine(opts)
       return obj.save()
     })
     .then(function (obj) {
-      return Q.fcall(function () {
-          var uData = userData || self.settings.ec2.userData
-          var payload = {
-            ImageId: self.settings.ec2.ami,
-            InstanceType: self.settings.ec2.type,
-            KeyName: self.settings.ec2.keyName,
-            MinCount: self.settings.ec2.min,
-            MaxCount: self.settings.ec2.max
-          }
-          if (self.settings.ec2.networkgroups.length ||
-              self.settings.ec2.subnet)
-            payload.NetworkInterfaces = [{
-              AssociatePublicIpAddress: true,
-              DeviceIndex: 0,
-              Groups: self.settings.ec2.networkgroups,
-              SubnetId: self.settings.ec2.subnet
-            }]
-          if (uData) payload.UserData = new Buffer(uData).toString("base64")
-          return payload
+      var uData = userData || self.settings.ec2.userData
+      var payload = {
+        ImageId: self.settings.ec2.ami,
+        InstanceType: self.settings.ec2.type,
+        KeyName: self.settings.ec2.keyName,
+        MinCount: self.settings.ec2.min,
+        MaxCount: self.settings.ec2.max
+      }
+      if (self.settings.ec2.networkgroups.length ||
+          self.settings.ec2.subnet)
+        payload.NetworkInterfaces = [{
+          AssociatePublicIpAddress: true,
+          DeviceIndex: 0,
+          Groups: self.settings.ec2.networkgroups,
+          SubnetId: self.settings.ec2.subnet
+        }]
+      if (uData) payload.UserData = new Buffer(uData).toString("base64")
+      return [obj, payload]
+    })
+    .spread(function (obj, payload) {
+      return spawnInstance(self.ec2, opts.name, payload,
+                           self.settings.retry)
+        .catch(function (err) {
+          return obj.remove()
+            .thenReject(err)
         })
-        .then(function (payload) {
-          return spawnInstance(self.ec2, opts.name, payload,
-                               self.settings.retry)
-            .catch(function (err) {
-              return obj.remove()
-                .then(function () {
-                  throw err
-                })
-            })
-            .then(function (res) {
-              var reservation = res.data.Reservations[0]
-              var instance = reservation.Instances[0]
-              var data = {
-                reservationId: reservation.ReservationId,
-                instanceId: instance.InstanceId
-              }
-              if (opts.extra) data.extra = opts.extra
-              return obj.update(data)
-                .then(function () {
-                        return self.getMachine(opts.name)
-                      },
-                      function (err) {
-                        return obj.remove()
-                          .then(function () {
-                            throw err
-                          })
-                      })
-            })
+        .then(function (awsMachine) {
+          return associateMachineToInstance(obj, awsMachine)
+        })
+        .then(function () {
+          return self.getMachineById(obj._id) // Return fresh data
         })
     })
 }
 
-var spawnInstance = function (ec2, name, payload, retry) {
-  return ec2.runInstances(payload).promise()
-    .then(function (res) {
-      return Q.delay(retry.delay)
-        .then(function () {
-          var instance = res.data.Instances[0]
-          return retrySetName(ec2, instance.InstanceId,
-                              {
-                                Name: name,
-                                SpawnedBy: "aws-machine-manager",
-                                Env: process.env.NODE_ENV || "development"
-                              },
-                              retry.delay, retry.count)
-            .catch(function (err) {
-              if (err instanceof exc.MaxRetriesError)
-                ec2.terminateInstances({InstanceIds: [instance.InstanceId]})
-              throw err
-            })
-            .then(function () {
-              var args = {InstanceIds: [instance.InstanceId]}
-              return ec2.describeInstances(args).promise()
-            })
-        })
+var associateMachineToInstance = function (obj, aws) {
+  var reservation = aws.data.Reservations[0]
+  var instance = reservation.Instances[0]
+  var data = {
+    reservationId: reservation.ReservationId,
+    instanceId: instance.InstanceId
+  }
+  return Q(obj.update(data)) // somehow .update doesn't return a Q promise?
+    .catch(function (err) {
+      return Q.all(
+        [
+          self.ec2.terminateInstances({InstanceIds: [data.instanceId]}),
+          obj.remove()
+        ])
+        .thenReject(err)
     })
 }
 
@@ -126,3 +97,30 @@ var retrySetName = function (ec2, instanceId, tags, delay, retries) {
       return res.data
     })
 }
+
+var spawnInstance = function (ec2, name, payload, retry) {
+  return ec2.runInstances(payload).promise()
+    .then(function (res) {
+      return Q.delay(retry.delay)
+        .then(function () {
+          var instance = res.data.Instances[0]
+          return retrySetName(ec2, instance.InstanceId,
+                              {
+                                Name: name,
+                                SpawnedBy: "aws-machine-manager",
+                                Env: process.env.NODE_ENV || "development"
+                              },
+                              retry.delay, retry.count)
+            .catch(function (err) {
+              if (err instanceof exc.MaxRetriesError)
+                ec2.terminateInstances({InstanceIds: [instance.InstanceId]})
+              throw err
+            })
+            .then(function () {
+              var args = {InstanceIds: [instance.InstanceId]}
+              return ec2.describeInstances(args).promise()
+            })
+        })
+    })
+}
+
